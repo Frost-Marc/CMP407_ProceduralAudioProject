98324ba2c7bafe98812ce0a4b568f849d0af9aee
Frost-Marc <124355875+Frost-Marc@users.noreply.github.com>
2026-03-05 10:22:22 +0000

    small tests
    small tests to see about basic sound generation that didnt work and small test to try get imgui but still need to integrate it first

---
+++ CMP407-ProceduralAudio/CMP407-ProceduralAudio/FireAudio.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Audio.hpp>
+
+class FireAudio : public sf::SoundStream
+{
+public:
+
+private:
+
+};
--- CMP407-ProceduralAudio/CMP407-ProceduralAudio/Main.cpp
+++ CMP407-ProceduralAudio/CMP407-ProceduralAudio/Main.cpp
@@ -4,12 +4,13 @@
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode({ 1000, 800 }), "ProceduralAudio");
+	sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "ProceduralAudio");
+	window.setFramerateLimit(144);
 
 	sf::CircleShape Fire;
 	Fire.setRadius(50.f);
 	Fire.setFillColor(sf::Color::Yellow);
-	Fire.setPosition({ 200,200 });
+	Fire.setPosition({ 200 - Fire.getRadius(), 200 - Fire.getRadius() });
 
 	while (window.isOpen())
 	{
@@ -22,7 +23,13 @@ int main()
 		window.clear();
 		window.draw(Fire);
 		window.display();
+		gui();
 	}
 
 	return 0;
+}
+
+void gui()
+{
+
 }
