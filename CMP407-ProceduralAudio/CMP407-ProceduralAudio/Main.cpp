acc4fbaff622846e4a4d41eb9abb3dbd8221635e
Frost-Marc <124355875+Frost-Marc@users.noreply.github.com>
2026-03-05 09:05:32 +0000

    full code reset
    full reset the project to nothing so i can no work on the proper implementation after the previous tests

--- CMP407-ProceduralAudio/CMP407-ProceduralAudio/Main.cpp
+++ CMP407-ProceduralAudio/CMP407-ProceduralAudio/Main.cpp
@@ -4,18 +4,5 @@
 
 int main()
 {
-    FireSoundGen fire;
-
-    fire.setIntensity(0.8f);
-    fire.setCrackleRate(25.0f);
-    fire.setBrightness(0.6f);
-    fire.setFlickerSpeed(0.4f);
-
-    fire.play();
-
-    std::cout << "Procedural Fire Running...\n";
-    std::cout << "Press Enter to quit.\n";
-    std::cin.get();
-
 	return 0;
 }
