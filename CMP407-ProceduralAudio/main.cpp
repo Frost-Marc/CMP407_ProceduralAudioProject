8937640c46f1a39fcee17013ba57b392cb1023a1
Frost-Marc <124355875+Frost-Marc@users.noreply.github.com>
2026-04-07 15:09:44 +0100

    Imgui integration
    working imgui integration with a basic imgui box

--- CMP407-ProceduralAudio/main.cpp
+++ CMP407-ProceduralAudio/main.cpp
@@ -1,3 +1,6 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 #include <SFML/System.hpp>
 #include "SFML/Graphics.hpp"
 #include <iostream>
@@ -6,26 +9,38 @@ int main()
 {
 	sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "Procedural-Audio");
 	window.setFramerateLimit(144);
+	ImGui::SFML::Init(window);
 
 	sf::CircleShape fire;
 	fire.setRadius(50.f);
 	fire.setFillColor(sf::Color::Yellow);
 	fire.setPosition({ 200 - fire.getRadius(), 200 - fire.getRadius() });
 
+	sf::Clock deltaClock;
 	while (window.isOpen())
 	{
-		while (const std::optional event = window.pollEvent())
+		while (const auto event = window.pollEvent())
 		{
+			ImGui::SFML::ProcessEvent(window, *event);
+
 			if (event->is<sf::Event::Closed>())
 			{
 				window.close();
 			}
 		}
 
+		ImGui::SFML::Update(window, deltaClock.restart());
+
+		ImGui::Begin("Fire Settings");
+		ImGui::Text("TEXT!");
+		ImGui::End();
+
 		window.clear();
 		window.draw(fire);
+		ImGui::SFML::Render(window);
 		window.display();
 	}
 
+	ImGui::SFML::Shutdown();
 	return 0;
 }
